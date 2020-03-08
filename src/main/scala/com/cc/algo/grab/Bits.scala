9b2c57e3ffb758403d0dec6a18efa6a466586021
cenkcorapci <cenk.corapci@gmail.com>
2020-03-08 10:01:57 +0300

    Implement grab challenges

--- src/main/scala/com/cc/algo/grab/Bits.scala
+++ src/main/scala/com/cc/algo/grab/Bits.scala
@@ -1,43 +1,88 @@
 package com.cc.algo.grab
 
 object Bits extends App {
+
+
   def solution(a: Array[Int], b: Array[Int]): Array[Int] = {
-    a.zipAll(b, 0, 0).zipWithIndex.map {
-      case ((first, second), i) =>
-        val force = math.pow(-2, i).toInt
-        first * force + second * force
+    val arr1 = a.reverse
+    val arr2 = b.reverse
+
+    val len1 = arr1.length
+    val len2 = arr2.length
+    if (len1 == 0) return arr2
+    if (len2 == 0) return arr1
+    val maxLen = (if (len1 > len2) len1
+    else len2) + 2
+    val result = new Array[Int](maxLen)
+    var carry = 0
+    var i = 0
+    // deal with the common part where both the arrays has numbers
+    while ( {
+      len1 - 1 - i >= 0 && len2 - 1 - i >= 0
+    }) {
+      val sum = arr1(len1 - 1 - i) + arr2(len2 - 1 - i) + carry
+      result(maxLen - 1 - i) = getBit(sum)
+      carry = getCarry(sum)
+      i += 1
     }
-
-  }
-
-  val a = Array(0, 1, 0)
-  val b = Array(1, 0, 1)
-  val s = solution(a, b)
-  s.map(println)
-  println(NegativeBase.intToStr(s.sum, -2))
-}
-
-object NegativeBase {
-  val digits = ('0' to '9') ++ ('a' to 'z') ++ ('A' to 'Z')
-
-  def intToStr(n: Int, b: Int): String = {
-    def _fromInt(n: Int): List[Int] = {
-      if (n == 0) {
-        Nil
-      } else {
-        val r = n % b
-        val rp = if (r < 0) r + b else r
-        val m = -(n - rp) / b
-        rp :: _fromInt(m)
-      }
+    // deal with the rest part
+    while ( {
+      len1 - 1 - i >= 0 && maxLen - 1 - i >= 0
+    }) {
+      val sum = arr1(len1 - 1 - i) + carry
+      result(maxLen - 1 - i) = getBit(sum)
+      carry = getCarry(sum)
+      i += 1
     }
+    while ( {
+      len2 - 1 - i >= 0 && maxLen - 1 - i >= 0
+    }) {
+      val sum = arr2(len2 - 1 - i) + carry
+      result(maxLen - 1 - i) = getBit(sum)
+      carry = getCarry(sum)
+      i += 1
+    }
+    // deal with the remaining carry
+    while ( {
+      maxLen - 1 - i >= 0 && carry != 0
+    }) {
+      result(maxLen - 1 - i) = getBit(carry)
+      carry = getCarry(carry)
+      i += 1
+    }
+    // remove the lead zeros
+    var zeroLeadCount = 0
+    while ( {
+      zeroLeadCount < maxLen && result(zeroLeadCount) == 0
+    }) zeroLeadCount += 1
+    if (zeroLeadCount == maxLen) return Array[Int](0)
+    val res = new Array[Int](maxLen - zeroLeadCount)
+    var k = 0
+    while ( {
+      k + zeroLeadCount < maxLen
+    }) {
+      res(k) = result(k + zeroLeadCount)
 
-    _fromInt(n).map(digits).reverse.mkString
+      k += 1
+    }
+    res.reverse
   }
 
-  def strToInt(s: String, b: Int): Int = {
-    s.map(digits.indexOf).foldRight((0, 1)) { case (x, (sum, pow)) =>
-      (sum + x * pow, pow * -b)
-    }._1
+  // get carry according to the addition result number
+  private def getCarry(num: Int): Int = {
+    if (num == 2 || num == 3) return -1
+    if (num == -1) return 1
+    0
   }
+
+  // get bit according to the addition result number
+  private def getBit(num: Int): Int = {
+    if (num == 2) return 0
+    if (num == 3 || num == -1 || num == 1) return 1
+    0
+  }
+
+  val a = Array(0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1)
+  val b = Array(0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1)
+  solution(a, b).foreach(println)
 }
---
+++ src/main/scala/com/cc/algo/grab/LinkedListJump.scala
@@ -0,0 +1,21 @@
+package com.cc.algo.grab
+
+import scala.annotation.tailrec
+import scala.util.Try
+
+object LinkedListJump extends App {
+  def solution(a: Array[Int]): Int = {
+    @tailrec
+    def s(go: Int = 0, counter: Int = 0): Int =
+      Try(a(go)).toOption match {
+        case Some(nextIndex) =>
+          if (nextIndex == -1) counter + 1
+          else s(nextIndex, counter + 1)
+        case None => counter
+      }
+
+    s()
+  }
+
+  print(solution(Array(1, 4, -1, 3, 2)))
+}
