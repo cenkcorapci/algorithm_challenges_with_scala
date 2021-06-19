0ecb90f6631c9d642259d99c996f9759f2c7565b
cenk.corapci@cimri.com <cenk.corapci@cimri.com>
2021-06-19 11:04:57 +0300

    Letter Combinations of a Phone Number

---
+++ src/main/scala/com/leetcode/challenges/medium/LetterCombinationsOfAPhoneNumber.scala
@@ -0,0 +1,17 @@
+package org.cenkcorapci.challenges
+package com.leetcode.challenges.medium
+
+object LetterCombinationsOfAPhoneNumber extends App {
+  val alphabet = "abc,def,ghi,jkl,mno,pqrs,tuv,wxyz".split(",").map(_.toCharArray)
+
+  def letterCombinations(digits: String, combinations: Array[String] = Array.empty): List[String] =
+    if (digits.isEmpty) combinations.toList
+    else {
+      val letters = alphabet(digits.head.toString.toInt - 2)
+      if (combinations.isEmpty) letterCombinations(digits.tail, letters.map(_.toString))
+      else letterCombinations(digits.tail,
+        combinations.flatMap(previous => letters.map(next => previous + next)))
+    }
+
+  println(letterCombinations("23") == List("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"))
+}
