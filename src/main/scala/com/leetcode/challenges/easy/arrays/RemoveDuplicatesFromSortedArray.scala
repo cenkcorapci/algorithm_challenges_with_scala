a1ce3cf918f7ef9b0a18299ae8f211d90f876d9d
cenk.corapci@cimri.com <cenk.corapci@cimri.com>
2021-11-13 13:48:45 +0300

    Remove duplicates from sorted array

--- src/main/scala/com/leetcode/challenges/easy/arrays/RemoveDuplicatesFromSortedArray.scala
+++ src/main/scala/com/leetcode/challenges/easy/arrays/RemoveDuplicatesFromSortedArray.scala
@@ -1,5 +1,11 @@
 package com.leetcode.challenges.easy.arrays
 
-object RemoveDuplicatesFromSortedArray extends App {
+import scala.annotation.tailrec
 
+object RemoveDuplicatesFromSortedArray extends App {
+  @tailrec
+  def removeDuplicates(nums: Array[Int], acc: Array[Int] = Array.empty): Array[Int] =
+    if (nums.isEmpty) acc
+    else removeDuplicates(nums.tail, if (acc.headOption.contains(nums.head))
+      acc else acc :+ nums.head)
 }
